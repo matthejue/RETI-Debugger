0477748d001b7393a60892f354bf657ca7f66cf5
areotwister <areotwister@gmail.com>
2024-02-16 10:31:24 +0100

    solved bug where leaving a menu with escape causes one to be stuck because of the statemachine

--- lua/reti-debugger/windows.lua
+++ lua/reti-debugger/windows.lua
@@ -262,7 +262,6 @@ M.menu_modes = Menu(popup_options_menu, {
 			set_no_scrollbind()
 			M.window_titles_memory_focus()
 		end
-    state.delta_windows("popup closed")
 	end,
 	should_skip_item = function(item)
 		if item.id == state.scrolling_mode then
@@ -271,6 +270,9 @@ M.menu_modes = Menu(popup_options_menu, {
 			return false
 		end
 	end,
+  on_close = function()
+    state.delta_windows("popup closed")
+  end
 })
 
 
@@ -356,6 +358,9 @@ M.menu_examples = Menu(popup_options_menu, {
 			return false
 		end
 	end,
+  on_close = function()
+    state.delta_windows("popup closed")
+  end
 })
 
 return M
