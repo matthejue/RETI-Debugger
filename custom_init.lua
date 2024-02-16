1915fd3254c98a4eeeaa79aaab30e803fb9c18b2
areotwister <areotwister@gmail.com>
2024-02-16 06:31:47 +0100

    corrected custom_init.lua to use opts instead of config

--- custom_init.lua
+++ custom_init.lua
@@ -1,49 +1,47 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 
 local plugins = {
-  {
-    "matthejue/RETI-Debugger",
-    dependencies = { "MunifTanjim/nui.nvim" },
-    config = function()
-      require("reti-debugger").setup({
-        keys = {
-          load_example = "<leader>rl",
-          compile = "<leader>rc",
-          start = "<leader>rs",
-          hide = "<leader>rh"
-        },
-      })
-    end,
-  },
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-    opts = {}
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    config = function()
-      vim.cmd.colorscheme("kanagawa-wave")
-    end
-  }
+	{
+		"matthejue/RETI-Debugger",
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {
+			keys = {
+				load_example = "<leader>rl",
+				compile = "<leader>rc",
+				start = "<leader>rs",
+				hide = "<leader>rh",
+			},
+		},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {},
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		config = function()
+			vim.cmd.colorscheme("kanagawa-wave")
+		end,
+	},
 }
 
 require("lazy").setup(plugins)
